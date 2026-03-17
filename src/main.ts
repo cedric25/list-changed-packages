063ddf2333604efce1136c936174d4139923baf4
Cedric Nicoloso <cnicoloso@kpler.com>
2026-03-17 10:20:49 +0100

    stop using @actions/core, dist file from 28k lines to 110

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import * as core from '@actions/core'
+import * as fs from 'fs'
 import { getChangedPackages } from './getChangedPackages.js'
+
+function setOutput(name: string, value: string | boolean): void {
+  const outputFile = process.env['GITHUB_OUTPUT'] ?? ''
+  if (outputFile) {
+    const delimiter = `ghadelimiter_${crypto.randomUUID()}`
+    fs.appendFileSync(
+      outputFile,
+      `${name}<<${delimiter}\n${value}\n${delimiter}\n`
+    )
+  }
+}
 
 /**
  * The main function for the action.
@@ -8,24 +19,27 @@ import { getChangedPackages } from './getChangedPackages.js'
  */
 export async function run(): Promise<void> {
   try {
-    core.debug('Starting...')
+    console.log('::debug::Starting...')
 
     const { changedPackages, hasLockChanged } = getChangedPackages()
 
-    core.setOutput('changed_packages', changedPackages.join('\n'))
-    core.setOutput('changed_packages_one_line', changedPackages.join(' '))
-    core.setOutput(
+    setOutput('changed_packages', changedPackages.join('\n'))
+    setOutput('changed_packages_one_line', changedPackages.join(' '))
+    setOutput(
       'pnpm_filters_changed_packages',
       changedPackages.map((packageName) => `--filter ${packageName}`).join(' ')
     )
-    core.setOutput(
+    setOutput(
       'pnpm_filters_changed_and_down_packages',
       changedPackages
         .map((packageName) => `--filter ${packageName}...`)
         .join(' ')
     )
-    core.setOutput('has_lock_file_changed', hasLockChanged)
+    setOutput('has_lock_file_changed', hasLockChanged)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      console.log(`::error::${error.message}`)
+      process.exitCode = 1
+    }
   }
 }
