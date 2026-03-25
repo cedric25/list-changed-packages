62fe47b1a2a67c31e2e92a6d67d695896163bb0a
Cedric Nicoloso <cnicoloso@kpler.com>
2026-03-25 09:18:18 +0100

    eslint config a bit more up-to-date

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,21 @@
 // See: https://eslint.org/docs/latest/use/configure/configuration-files
 
-import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
+import tseslint from 'typescript-eslint'
 import jest from 'eslint-plugin-jest'
 import prettier from 'eslint-plugin-prettier'
 import globals from 'globals'
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-})
-
-export default [
+export default tseslint.config(
   {
     ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules']
   },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended'
-  ),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     plugins: {
       jest,
-      prettier,
-      '@typescript-eslint': typescriptEslint
+      prettier
     },
 
     languageOptions: {
@@ -40,7 +26,6 @@ export default [
         SharedArrayBuffer: 'readonly'
       },
 
-      parser: tsParser,
       ecmaVersion: 2023,
       sourceType: 'module',
 
@@ -85,4 +70,4 @@ export default [
       '@typescript-eslint/no-explicit-any': 'off'
     }
   }
-]
+)
